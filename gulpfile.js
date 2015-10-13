d1f6822bf6e65c36a9e8c5396fe790a4243965cc
javier <salomeka4@gmail.com>
2015-10-13 15:57:46 -0500

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp')  
+var webserver = require('gulp-webserver')  
+var browserify = require('browserify')  
+var babelify = require('babelify')  
+var source = require('vinyl-source-stream')  
+ 
+
+
+gulp.task('server', function() {  
+  gulp.src('./build')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      port: 8080,
+      fallback: 'index.html',
+      livereload: true
+    }))
+})
+
+
+
+gulp.task('build', function() {  
+  browserify({
+    entries: './src/index.js',
+    extensions: ['.js'],
+    debug: true
+  })
+  .transform(babelify)
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('./build/js'))
+})
+
+gulp.task('watch', function() {  
+  gulp.watch('./src/**/*.js', ['build'])
+  
+})
+
+gulp.task('default', ['server', 'watch']) 
